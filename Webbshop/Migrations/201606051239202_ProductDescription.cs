fb248445dfec6da8b66c52f548aa57ba91fe112d
dewzor <bergla84@hotmail.com>
2016-06-05 14:42:44 +0200

    uppdateradModel

---
+++ Webbshop/Migrations/201606051239202_ProductDescription.cs
@@ -0,0 +1,16 @@
+namespace Webbshop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ProductDescription : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- Webbshop/Models/Product.cs
+++ Webbshop/Models/Product.cs
@@ -12,5 +12,6 @@ namespace Webbshop.Models
         public Category Category { get; set; }
         public decimal Price { get; set; }
         public string ImageUrl { get; set; }
+        public string Description { get; set; }
     }
 }
